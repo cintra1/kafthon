81a221f37c9a420a2c5af6119f2d2d22c81c6e10
Mateus Cintra <mateusdade@gmail.com>
2024-09-15 17:07:48 -0300

    feat: send message

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def main():
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     server.accept() # wait for client
+    server.send(b"Hello, World!")	# send data to client
 
 
 if __name__ == "__main__":
