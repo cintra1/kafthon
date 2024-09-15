b199ee4dd893d040e50d2ef75c7ea5e8bcd94fdb
Mateus Cintra <mateusdade@gmail.com>
2024-09-15 17:09:32 -0300

    feat: send message

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import socket  # noqa: F401
 
 def main():
     print("Logs from your program will appear here!")
+    data = 7
+    data = data.to_bytes(4, byteorder='big')
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     server.accept() # wait for client
-    server.send(b"Hello, World!")	# send data to client
+    server.send(data)	# send data to client
 
 
 if __name__ == "__main__":
