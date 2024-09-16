7cbaf1ed912bddb6fbd2ede9622b40f5407dfe34
Mateus Cintra <mateusdade@gmail.com>
2024-09-16 20:20:56 -0300

    feat: add correlation id

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
 
+def create_message(data):
+    return data.to_bytes(4, byteorder='big')
+
+def handle_client(conn):
+    with conn:
+        print("Handling client")
+        data = 7 # conn.recv(1024)
+        conn.sendall(create_message(data))
+
 
 def main():
     print("Logs from your program will appear here!")
@@ -8,7 +17,12 @@ def main():
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     conn, addr = server.accept() # wait for client
-    print(7)
+    print('Connected by', addr)
+
+     with conn:
+        # Handle the incoming request
+        handle_client(conn)
+                
 
 if __name__ == "__main__":
     main()
