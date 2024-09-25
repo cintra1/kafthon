38e4bfec744b75d9c65867a6be8e7f445b409f06
Mateus Cintra <mateusdade@gmail.com>
2024-09-24 22:29:44 -0300

    feat: add fetch with no topics

--- app/main.py
+++ app/main.py
@@ -37,10 +37,11 @@ def make_response(api_key, api_version, correlation_id):
         fetch.to_bytes(2, byteorder='big') +
         min_fetch_version.to_bytes(2, byteorder='big') +
         max_fetch_version.to_bytes(2, byteorder='big') +
-        session_id.to_bytes(4, byteorder='big') +
-        response_body +
         tag_buffer +
         throttle_time_ms.to_bytes(4, byteorder='big') +
+        tag_buffer +
+        session_id.to_bytes(4, byteorder='big') +
+        response_body +
         tag_buffer
     )
 
