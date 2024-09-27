2492906d4e4780d368b4383300455b54ad32d94a
Mateus Cintra <mateusdade@gmail.com>
2024-09-26 21:00:17 -0300

    feat: add fetch with one topic

--- app/main.py
+++ app/main.py
@@ -64,8 +64,8 @@ def make_fetch_response(api_key, api_version, correlation_id):
     tag_buffer = b"\x00"
 
     response_body = (
-        throttle_time_ms.to_bytes(4, byteorder='big') +
         error_code.to_bytes(2, byteorder='big') +
+        throttle_time_ms.to_bytes(4, byteorder='big') +
         session_id.to_bytes(2, byteorder='big') +
         fetch.to_bytes(2, byteorder='big') +
         int(0).to_bytes(2, byteorder='big') +
